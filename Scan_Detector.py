ce37a129b8587924129595157d9f30c0ee47dd84
ss4cole <56775125+ss4cole@users.noreply.github.com>
2024-07-02 19:03:56 -0400

    Update Scan_Detector.py

--- Scan_Detector.py
+++ Scan_Detector.py
@@ -2,13 +2,12 @@
 Name: Scan Detector
 Goal: Parse PCAP files with python to look for common scanning methods
 
-[X]1. TCP SYN Scan
+[X]1. TCP SYN Scan (SS)
         open port   closed port
         0x0002      0x0002
         0x0012      0x0014
-        0x0010
-        0x0014
-[]2. TCP Connect Scan
+        0x0004
+[]2. TCP ACK Scan (SA)
 []3. UDP Scan
 
 '''
@@ -40,33 +39,63 @@ print(str(num_of_pkt)+" packets in pcap")
 #calc num of streams in pcap
 streams=0
 packet_range=range(0,num_of_pkt,1)
-
 for pkt in packet_range:
-        stream = int(pcap[pkt].tcp.stream)
-        streams = stream + 1
-        
+        try:        
+                stream = int(pcap[pkt].tcp.stream)
+                streams = stream + 1
+        except:
+                pass
 print("Number of Streams in pcap: "+ str(streams-1))
 
 #allign streams to all packets in stream
 stream_pkt_dict={}
 stream_range=range(0,streams,1)
 for pkt in packet_range:
-        key=pcap[pkt].tcp.stream
-        stream_pkt_dict.setdefault(key, [])
-        stream_pkt_dict[key].append(pcap[pkt].frame_info.number)
-
-print("[+]Checking pcap for TCP Scan...")
-
+        try:
+                key=pcap[pkt].tcp.stream
+                stream_pkt_dict.setdefault(key, [])
+                stream_pkt_dict[key].append(pcap[pkt].frame_info.number)
+        except:
+                pass
+print("[+]Checking for TCP SYN Scan...")
 for stream in stream_range:
-        open_port_scan = ['0x0002', '0x0012', '0x0004']
-        closed_port_scan=['0x0002', '0x0014']
-        tmp_string = []
-        temp_stream = stream_pkt_dict[str(stream)]
+        ss_open_port_scan=['0x0002', '0x0012', '0x0004']
+        ss_closed_port_scan=['0x0002', '0x0014']
+        tmp_string=[]
+        temp_stream=stream_pkt_dict[str(stream)]
         for pkt in temp_stream:
                 pkt=int(pkt)-1
                 flag = pcap[pkt].tcp.flags
                 tmp_string.append(flag)
-        if tmp_string == open_port_scan:
-                print("TCP Syn Scan of open port detected in stream " + str(stream))
-        if tmp_string == closed_port_scan:
-                print("TCP Syn Scan of closed port detected in stream " + str(stream))
+                if tmp_string==ss_open_port_scan:
+                        print("TCP Syn Scan of open port detected in stream "+str(stream))
+                if tmp_string==ss_closed_port_scan:
+                        print("TCP Syn Scan of closed port detected in stream "+str(stream))
+
+
+print('[+]Checking for TCP ACK Scan...')
+for stream in stream_range:
+        sa_port_scan=['0x0010', '0x0004']
+        tmp_string=[]
+        temp_stream=stream_pkt_dict[str(stream)]
+        for pkt in temp_stream:
+                pkt=int(pkt)-1
+                flag=pcap[pkt].tcp.flags
+                tmp_string.append(flag)
+                if tmp_string==sa_port_scan:
+                        print("TCP ACK Scan of open port detected in stream "+str(stream))
+
+print("[+]Checking for TCP FIN Scan...")
+for stream in stream_range:
+        sf_open_port_scan=['0x0001', '0x0014']
+        sf_closed_port_scan=['0x0001']
+        tmp_string=[]
+        temp_stream=stream_pkt_dict[str(stream)]
+        for pkt in temp_stream:
+                pkt=int(pkt)-1
+                flag=pcap[pkt].tcp.flags
+                tmp_string.append(flag)
+                if tmp_string==sf_open_port_scan:
+                        print("TCP FIN Scan of open port detected in stream "+str(stream))
+                if tmp_string==sf_closed_port_scan:
+                        print("TCP FIN Scan of closed port detected in stream "+str(stream))
