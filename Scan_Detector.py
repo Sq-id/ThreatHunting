22cfb550c04784d4219a30ddd57b4151160d90d5
Sq-id <146359308+Sq-id@users.noreply.github.com>
2024-10-16 19:18:31 -0500

    Update Scan_Detector.py

--- Scan_Detector.py
+++ Scan_Detector.py
@@ -1,101 +1,95 @@
-'''
-Name: Scan Detector
-Goal: Parse PCAP files with python to look for common scanning methods
-
-[X]1. TCP SYN Scan (SS)
-        open port   closed port
-        0x0002      0x0002
-        0x0012      0x0014
-        0x0004
-[]2. TCP ACK Scan (SA)
-[]3. UDP Scan
-
-'''
-#Imports
 import pyshark
-import collection
-#Vars
-banner = """
-███████╗██╗   ██╗ ██████╗██╗  ██╗    ██████╗ ██╗   ██╗████████╗██╗███╗   ██╗
-██╔════╝██║   ██║██╔════╝██║ ██╔╝    ██╔══██╗██║   ██║╚══██╔══╝██║████╗  ██║
-█████╗  ██║   ██║██║     █████╔╝     ██████╔╝██║   ██║   ██║   ██║██╔██╗ ██║
-██╔══╝  ██║   ██║██║     ██╔═██╗     ██╔═══╝ ██║   ██║   ██║   ██║██║╚██╗██║
-██║     ╚██████╔╝╚██████╗██║  ██╗    ██║     ╚██████╔╝   ██║   ██║██║ ╚████║
-╚═╝      ╚═════╝  ╚═════╝╚═╝  ╚═╝    ╚═╝      ╚═════╝    ╚═╝   ╚═╝╚═╝  ╚═══╝
-                                                                            
-"""
-
+import psutil
+from termcolor import colored
+banner = colored("""
+--------------------------------------------------------------
+░▒▓███████▓▒░░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░▒▓████████▓▒░      
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          
+░▒▓███████▓▒░░▒▓█▓▒░    ░▒▓██████▓▒░░▒▓████████▓▒░ ░▒▓█▓▒░          
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          
+░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          
+---------------------------------------------------------------""",'green')
 print(banner)
-pcap_path=input("Enter the file for analysis: ")
-pcap=pyshark.FileCapture(pcap_path)
-#Main Logic
-
-#calc numb of packets in pcap
-num_of_pkt=0
-for pkt in pcap:
-        num_of_pkt=num_of_pkt+1
-print(str(num_of_pkt)+" packets in pcap")
-
-#calc num of streams in pcap
-streams=0
-packet_range=range(0,num_of_pkt,1)
-for pkt in packet_range:
-        try:        
-                stream = int(pcap[pkt].tcp.stream)
-                streams = stream + 1
-        except:
-                pass
-print("Number of Streams in pcap: "+ str(streams-1))
-
-#allign streams to all packets in stream
-stream_pkt_dict={}
-stream_range=range(0,streams,1)
-for pkt in packet_range:
-        try:
-                key=pcap[pkt].tcp.stream
-                stream_pkt_dict.setdefault(key, [])
-                stream_pkt_dict[key].append(pcap[pkt].frame_info.number)
-        except:
-                pass
-print("[+]Checking for TCP SYN Scan...")
-for stream in stream_range:
-        ss_open_port_scan=['0x0002', '0x0012', '0x0004']
-        ss_closed_port_scan=['0x0002', '0x0014']
-        tmp_string=[]
-        temp_stream=stream_pkt_dict[str(stream)]
-        for pkt in temp_stream:
-                pkt=int(pkt)-1
-                flag = pcap[pkt].tcp.flags
-                tmp_string.append(flag)
-                if tmp_string==ss_open_port_scan:
-                        print("TCP Syn Scan of open port detected in stream "+str(stream))
-                if tmp_string==ss_closed_port_scan:
-                        print("TCP Syn Scan of closed port detected in stream "+str(stream))
+print(colored("1. Analyze PCAP for NMAP Port Scans\n2. Live Analysis for NMAP Portscans\n3. print fuck\n---------------------------------------------------------------",'green'))
+Main_Choice=input(colored("Select analysis option: (1,2,3...): ",'green'))
 
 
-print('[+]Checking for TCP ACK Scan...')
-for stream in stream_range:
-        sa_port_scan=['0x0010', '0x0004']
-        tmp_string=[]
-        temp_stream=stream_pkt_dict[str(stream)]
-        for pkt in temp_stream:
-                pkt=int(pkt)-1
-                flag=pcap[pkt].tcp.flags
-                tmp_string.append(flag)
-                if tmp_string==sa_port_scan:
-                        print("TCP ACK Scan of open port detected in stream "+str(stream))
 
-print("[+]Checking for TCP FIN Scan...")
-for stream in stream_range:
-        sf_open_port_scan=['0x0001', '0x0014']
-        sf_closed_port_scan=['0x0001']
-        tmp_string=[]
-        temp_stream=stream_pkt_dict[str(stream)]
-        for pkt in temp_stream:
-                pkt=int(pkt)-1
-                flag=pcap[pkt].tcp.flags
-                tmp_string.append(flag)
-                if tmp_string==sf_open_port_scan:
-                        print("TCP FIN Scan of open port detected in stream "+str(stream))
-                if tmp_string==sf_closed_port_scan:
-                        print("TCP FIN Scan of closed port detected in stream "+str(stream))
+match Main_Choice:
+        case "1":
+                input_text=colored("Enter the file for analysis: ", 'green')
+                pcap_path=input(input_text)
+                pcap=pyshark.FileCapture(pcap_path)
+                #Main Logic
+                scan_detection_counter=0
+                scan_flags_dict={
+                        'Open SYN Scan':['0x0002', '0x0012', '0x0004'],
+                        'Closed SYN Scan':['0x0002', '0x0014'],
+                        'TCP ACK Scan':['0x0010', '0x0004'],
+                        'Open FIN Scan':['0x0001', '0x0014'],
+                        'Closed FIN Scan':['0x0001'],
+                }
+                #Functions
+                def Check_Protocol_And_Filter(pkt):
+                        Protocol=pcap[pkt].frame_info.protocols
+                        if "eth:ethertype:ip:tcp"==Protocol:
+                                return "TCP"
+                        if "eth:ethertype:ip:udp:data"==Protocol:
+                                return "UDP"
+                        else:
+                                return "0"
+                #Main Logic
+                #calc numb of packets in pcap
+                num_of_pkt=0
+                for pkt in pcap:
+                        num_of_pkt=num_of_pkt+1
+                packet_text=colored(" packets in pcap",'green')
+                print(str(colored(num_of_pkt,'green'))+ packet_text)
+                #filter tcp and udp from file into lists for analysis
+                packet_range=range(0,num_of_pkt,1)
+                tcp_pkt_to_stream_dict={}
+                udp_pkt_to_stream_dict={}
+                for pkt in packet_range:
+                        Protocol = Check_Protocol_And_Filter(pkt)
+                        if Protocol=="TCP":
+                                key=pcap[pkt].tcp.stream
+                                tcp_pkt_to_stream_dict.setdefault(key, [])
+                                tcp_pkt_to_stream_dict[key].append(pcap[pkt].frame_info.number)
+                        if Protocol=="UDP":
+                                key=pcap[pkt].udp.stream
+                                udp_pkt_to_stream_dict.setdefault(key, [])
+                                udp_pkt_to_stream_dict[key].append(pcap[pkt].frame_info.number)
+                for key in tcp_pkt_to_stream_dict.keys():
+                        pkts=tcp_pkt_to_stream_dict[key]
+                        flag_str=[]
+                        temp_stream=tcp_pkt_to_stream_dict[key]
+                        for p in temp_stream:
+                                p=int(p)-1
+                                flag=pcap[p].tcp.flags
+                                flag_str.append(flag)
+                        for scans in scan_flags_dict.keys():
+                                if flag_str == scan_flags_dict[scans]:
+                                        print(str(colored(scans,'green'))+colored(" Dectected in stream ",'green')+ colored(key,'green'))
+                                        scan_detection_counter = scan_detection_counter +1
+                if scan_detection_counter==0:
+                        print(colored("No Scans detected in PCAP :)",'green'))
+                else:
+                        print(colored("Number of Scans detected: ",'green')+str(colored(scan_detection_counter,'green')))
+        
+        case "2":
+                print(colored("Select Interface:",'green'))
+                interface=psutil.net_if_stats()
+                print(colored(interface,'green'))
+                interface=input(colored("Input Option: ",'green'))
+                capture=pyshark.LiveCapture(interface=interface)
+                capture.sniff(timeout=10)
+                for pkt in capture:
+                        try:
+                                print(colored(pkt.data,'green'))
+                        except:
+                                print(colored(pkt.frame_info.protocols,'green'))
+
+        case "3":
+                print(colored("Fuck",'green'))
